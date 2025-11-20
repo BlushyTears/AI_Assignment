0189b82800bc9de655c84eb268941f198457eddb
BlushyTears <m123.123.123@hotmail.com>
2025-11-20 22:01:34 +0100

    Clean up files and add jumping for part 3

---
+++ src/ComposedAgents.cpp
@@ -0,0 +1,374 @@
+#include "raylib.h"
+#include "raymath.h"
+#include "resource_dir.h"
+
+#include "Agent.h"
+#include "memory.h"
+#include <vector>
+#include "ComposedAgents.h"
+
+PathfollowAgent::PathfollowAgent(int _maximumPathCount) {
+	width = GetScreenWidth();
+	height = GetScreenHeight();
+	agent = new Agent(Vector2{ width / 2, height / 6 }, 15.0f, 5.0f, 0, 0.1f, true);
+	obj = new Object(Vector2{ width / 2, height / 4 }, 25.0f, 5.0f);
+	maximumPathCount = _maximumPathCount;
+	agent->behaviorImpl = std::make_unique<SeekBehavior>();
+}
+
+PathfollowAgent::~PathfollowAgent() {
+	delete agent;
+	delete obj;
+}
+
+void PathfollowAgent::update() {
+	generateNewPath();
+	updatePathFollowAgent();
+	obj->drawShape();
+	debugDraw();
+}
+
+void PathfollowAgent::generateNewPath() {
+	if (nodePositions.empty()) {
+		for (int i = 0; i < maximumPathCount; i++) {
+			float x = GetRandomValue(0, width);
+			float y = GetRandomValue(0, height);
+			nodePositions.push_back(Vector2{ x, y });
+		}
+		currentNodeIndex = 0;
+		if (!nodePositions.empty()) obj->position = nodePositions[0];
+	}
+}
+
+// I realized that since i am using unique pointer, that setting seek = std::make_unique<SeekBehavior>()
+// In the constructor would mean I need to use move() and thus now seek is equal nullptr and useless.
+// The ideal solution would be to use shared pointer or copy it as a raw pointer (Tedious) --
+// but that's beyond the scope of this assignment.
+void PathfollowAgent::updatePathFollowAgent() {
+	if (nodePositions.size() == 0)
+		return;
+
+	if (currentNodeIndex < 0) currentNodeIndex = 0;
+	if (currentNodeIndex >= nodePositions.size()) currentNodeIndex = 0;
+	Vector2 target = nodePositions[currentNodeIndex];
+
+	if (Vector2Length(agent->position - target) > 10) agent->updateFrame(obj);
+	else {
+		// Increment, then check if it's time to reset, and only after update obj position
+		currentNodeIndex++;
+		if (currentNodeIndex >= nodePositions.size()) {
+			currentNodeIndex = 0;
+			nodePositions.clear();
+			return;
+		}
+		obj->position = nodePositions[currentNodeIndex];
+	}
+}
+
+void PathfollowAgent::debugDraw() {
+	if (nodePositions.size() > 0) {
+		for (int i = 0; i < nodePositions.size(); i++) {
+			DrawCircleLines(nodePositions[i].x, nodePositions[i].y, 10, DARKPURPLE);
+
+			if (i < nodePositions.size() - 1) {
+				DrawLine(nodePositions[i].x, nodePositions[i].y,
+					nodePositions[i + 1].x, nodePositions[i + 1].y,
+					DARKPURPLE);
+			}
+		}
+	}
+}
+
+SeparatedAgents::SeparatedAgents() : numOfAgents(0), trackedObject(nullptr) {}
+
+SeparatedAgents::SeparatedAgents(int _numOfAgents) {
+	trackedObject = new Player(Vector2{ (float)GetScreenWidth() / 2, (float)GetScreenHeight() / 3 }, 25.0f, 5.0f);
+
+	numOfAgents = _numOfAgents;
+	for (int i = 0; i < numOfAgents; i++) {
+		// Ugly but needed way to spawn agents a bit randomly 
+		// (If they spawn inside of each other then they'll stay like that)
+		Agent* agent;
+		agent = new Agent(Vector2{ (float)GetScreenWidth() / 2, (float)GetScreenHeight() / 2 + (agentRadius * (i + 1)) },
+			agentRadius, 3.0f, 0, 0.1f, true);
+
+		// Set agent behavior (Can vary per agent) (Default behavior is seek but we explicitly define it here)
+		agentList.push_back(agent);
+	}
+}
+
+void SeparatedAgents::update() {
+	handleCollision();
+	if (trackedObject) {
+		trackedObject->Update();
+	}
+	for (Agent* a : agentList) {
+		a->updateFrame(trackedObject);
+	}
+	trackedObject->Update();
+}
+
+float SeparatedAgents::getMinDistance(int dist1, int dist2) {
+	return (dist1 + dist2) * 2;
+}
+
+void SeparatedAgents::handleCollision() {
+	for (int i = 0; i < agentList.size(); i++) {
+		for (int j = i + 1; j < agentList.size(); j++) {
+			Vector2 diff = agentList[i]->position - agentList[j]->position;
+			float distSq = diff.x * diff.x + diff.y * diff.y;
+
+			float minimumDistance = getMinDistance(agentList[i]->radius, agentList[j]->radius);
+			float minimumDistanceSqared = minimumDistance * minimumDistance;
+
+			if (distSq < minimumDistanceSqared) {
+				float dist = sqrtf(distSq);
+				Vector2 normal = diff / dist;
+
+				float penetration = (minimumDistance - dist) * 0.5f;
+				agentList[i]->position += normal * penetration;
+			}
+		}
+		agentList[i]->updateFrame(trackedObject);
+	}
+}
+
+SeparatedAgents::~SeparatedAgents() {
+	for (int i = 0; i < agentList.size(); i++) {
+		delete agentList[i];
+	}
+	delete trackedObject;
+}
+
+
+ObjectAvoidance::ObjectAvoidance(int numAgents) : SeparatedAgents(numAgents) {
+	dummyObject = new Object(trackedObject->position, trackedObject->radius, 0);
+	// Walls
+	walls.push_back(LineWall({ 150, 150 }, { 300, 150 }));
+	walls.push_back(LineWall({ 500, 400 }, { 500, 550 }));
+	walls.push_back(LineWall({ 400, 700 }, { 550, 750 }));
+
+	// Box aka Object
+	walls.push_back(LineWall({ 800, 200 }, { 1000, 200 }));
+	walls.push_back(LineWall({ 1000, 200 }, { 1000, 350 }));
+	walls.push_back(LineWall({ 1000, 350 }, { 800, 350 }));
+	walls.push_back(LineWall({ 800, 350 }, { 800, 200 }));
+}
+
+ObjectAvoidance::~ObjectAvoidance() {
+	delete dummyObject;
+}
+
+void ObjectAvoidance::update() {
+	avoidWalls();
+
+	SeparatedAgents::update();
+
+	trackedObject->Update();
+	trackedObject->drawShape();
+	drawWalls();
+}
+
+float ObjectAvoidance::getMinDistance(int dist1, int dist2) {
+	return dist1 + dist2;
+}
+
+void ObjectAvoidance::avoidWalls() {
+	for (Agent* _a : agentList) {
+		float rayLength = 150.0f;
+		float fovAngle = 45.0 * DEG2RAD;
+		bool raycastHit = false;
+
+		Vector2 ray = _a->position + Vector2Normalize(_a->forwardDirection) * rayLength;
+		Vector2 forward = Vector2Normalize(_a->forwardDirection);
+		Vector2 avoidDir = forward;
+
+		// There are plenty of ways of doing this but i decided to go with 
+		// one bigger raycast in the center and two smaller ones
+		Vector2 whiskers[3] = {
+			_a->position + forward * rayLength,
+			_a->position + Vector2Rotate(forward, +fovAngle) * rayLength / 2,
+			_a->position + Vector2Rotate(forward, -fovAngle) * rayLength / 2,
+		};
+
+		for (int i = 0; i < walls.size(); i++) {
+			Vector2 mid = (walls[i].start + walls[i].end) * 0.5f;
+			float dist = Vector2Distance(ray, mid);
+
+			for (int i = 0; i < 3; i++) {
+				float dist = Vector2Distance(whiskers[i], mid);
+				if (dist < _a->radius + 50.0f) {
+					avoidDir = Vector2Normalize(_a->position - mid);
+					raycastHit = true;
+					break;
+				}
+			}
+			if (raycastHit) break;
+		}
+
+		if (raycastHit) {
+			// change target position
+			dummyObject->position = _a->position + avoidDir * rayLength;
+			_a->playerTarget = dummyObject;
+		}
+		else
+			_a->playerTarget = trackedObject;
+	}
+}
+
+void ObjectAvoidance::drawWalls() {
+	DrawText("- I understand that the agents don't perfectly avoid the box in particular. The main issue is separation sometimes overriding it",
+		200, 10, 20, RED);
+	DrawText("- I think potentially blending the behaviors with weights that the book brings up or just having real collision for the walls would work",
+		200, 40, 20, RED);
+	DrawText("- But if the agents are far enough from the player and move toward one of the box corners it works quite well",
+		200, 70, 20, RED);
+
+	for (int i = 0; i < walls.size(); i++) {
+		DrawLine(walls[i].start.x, walls[i].start.y, walls[i].end.x, walls[i].end.y, GREEN);
+	}
+}
+
+Pad::Pad(Vector2 _position, Vector2 _size) {
+	position = _position;
+	size = _size;
+}
+
+bool Pad::isAgentOnThisPad(Agent* agent, float padRadius) {
+	float cx = agent->position.x;
+	float cy = agent->position.y;
+	float r = padRadius;
+
+	float xMin = position.x;
+	float xMax = position.x + size.x;
+	float yMin = position.y;
+	float yMax = position.y + size.y;
+
+	if (cx + r >= xMin
+		&& cx - r <= xMax
+		&& cy + r >= yMin
+		&& cy - r <= yMax) {
+		return true;
+	}
+	return false;
+}
+
+void Pad::drawPad(Color _color) {
+	DrawRectangle(position.x, position.y, size.x, size.y, _color);
+}
+
+JumpingAgent::JumpingAgent() {
+	agent = new Agent(Vector2{ (float)GetScreenWidth() / 6, (float)GetScreenHeight() / 4 }, 25.0f, 5.0f, 0, 0.1f, true);
+	player = new Player(Vector2{ (float)GetScreenWidth() / 1.2f, (float)GetScreenHeight() / 2 }, 25.0f, 8.0f);
+	pad = new Pad(Vector2{ (float)GetScreenWidth() / 2, (float)GetScreenHeight() / 5 }, Vector2{ 50, 150 });
+	deathPad = new Pad(Vector2{ (float)GetScreenWidth() / 1.85f, (float)GetScreenHeight() / 6 }, Vector2{ 100, 450 });
+	hasJumped = false;
+	jumpAcceleration = { 0, 0 };
+	gravity = 0.4f;
+	zPosition = 0.0f;
+	jumpSpeed = 0;
+	baseRadius = agent->radius;
+}
+
+JumpingAgent::~JumpingAgent() {
+	delete agent;
+	delete player;
+	delete pad;
+}
+
+void JumpingAgent::jump() {
+	if (!hasJumped) {
+		jumpSpeed = 10.0f;
+		hasJumped = true;
+	}
+}
+
+void JumpingAgent::respawn() {
+	agent->position = Vector2{ (float)GetScreenWidth() / 6, (float)GetScreenHeight() / 4 };
+}
+
+void JumpingAgent::applyJumpPhysics() {
+	if (hasJumped) {
+		zPosition += jumpSpeed;
+		jumpSpeed -= gravity;
+
+		if (zPosition <= 0.0f) {
+			zPosition = 0.0f;
+			jumpSpeed = 0.0f;
+			hasJumped = false;
+			agent->radius = baseRadius;
+		}
+		else {
+			float scale = 1.0f + (zPosition * 0.008f);
+			agent->radius = baseRadius * scale;
+		}
+	}
+}
+
+void JumpingAgent::checkPads() {
+	if (!hasJumped && zPosition <= 0.0f && pad->isAgentOnThisPad(agent, baseRadius)) {
+		jump();
+	}
+	if (deathPad->isAgentOnThisPad(agent, baseRadius) && zPosition <= 0.0f)
+		respawn();
+}
+
+void JumpingAgent::drawPads() {
+	pad->drawPad(Color{ 100, 100, 100, 105 });
+	deathPad->drawPad(Color{ 255, 50, 50, 105 });
+}
+
+void JumpingAgent::update() {
+	checkPads();
+	drawPads();
+	applyJumpPhysics();
+	agent->updateFrame(player);
+	player->Update();
+}
+
+ComposedAgents::ComposedAgents() {
+	currentBehavior = Pathfollow;
+
+	pathFollowBehavior = new PathfollowAgent(5);
+	separatedAgentsBehavior = new SeparatedAgents(5);
+	collisionAvoidanceBehavior = new ObjectAvoidance(5);
+	jumpingBehavior = new JumpingAgent();
+}
+
+ComposedAgents::~ComposedAgents() {
+	delete pathFollowBehavior;
+	delete separatedAgentsBehavior;
+	delete collisionAvoidanceBehavior;
+	delete jumpingBehavior;
+}
+
+void ComposedAgents::displayDebug() {
+	switch (currentBehavior) {
+	case Pathfollow:
+		pathFollowBehavior->update();
+		DrawText("1/4 Type: Pathfollow", 10, GetScreenHeight() - 50, 20, RED);
+		break;
+	case AgentSeparation:
+		separatedAgentsBehavior->update();
+		DrawText("2/4 Type: Separated Agents Behavior", 10, GetScreenHeight() - 50, 20, RED);
+		break;
+	case CollisionAvoidance:
+		collisionAvoidanceBehavior->update();
+		DrawText("3/4 Type: Collision, agent and Wall Avoidance", 10, GetScreenHeight() - 50, 20, RED);
+		break;
+	case AgentJumping:
+		jumpingBehavior->update();
+		DrawText("4/4 Type: Jumping", 10, GetScreenHeight() - 50, 20, RED);
+		break;
+	default:
+		DrawText("Error, most likely invalid option picked", 10, GetScreenHeight(), 20, RED);
+		break;
+	}
+}
+
+void ComposedAgents::browseStates() {
+	if (IsKeyPressed(KEY_ONE)) currentBehavior = Pathfollow;
+	if (IsKeyPressed(KEY_TWO)) currentBehavior = AgentSeparation;
+	if (IsKeyPressed(KEY_THREE)) currentBehavior = CollisionAvoidance;
+	if (IsKeyPressed(KEY_FOUR)) currentBehavior = AgentJumping;
+}
--- src/ComposedAgents.h
+++ src/ComposedAgents.h
@@ -29,77 +29,12 @@ struct PathfollowAgent {
 	float width;
 	float height;
 
-	PathfollowAgent(int _maximumPathCount) {
-		width = GetScreenWidth();
-		height = GetScreenHeight();
-		agent = new Agent(Vector2{ width / 2, height / 6 }, 15.0f, 5.0f, 0, 0.1f, true);
-		obj = new Object(Vector2{ width / 2, height / 4 }, 25.0f, 5.0f);
-		maximumPathCount = _maximumPathCount;
-		agent->behaviorImpl = std::make_unique<SeekBehavior>();
-	}
-
-	void update() {
-		generateNewPath();
-		updatePathFollowAgent();
-		obj->drawShape();
-		debugDraw();
-	}
-
-	void generateNewPath() {
-		if (nodePositions.empty()) {
-			for (int i = 0; i < maximumPathCount; i++) {
-				float x = GetRandomValue(0, width);
-				float y = GetRandomValue(0, height);
-				nodePositions.push_back(Vector2{ x, y });
-			}
-			currentNodeIndex = 0;
-			if (!nodePositions.empty()) obj->position = nodePositions[0];
-		}
-	}
-
-	// I realized that since i am using unique pointer, that setting seek = std::make_unique<SeekBehavior>()
-	// In the constructor would mean I need to use move() and thus now seek is equal nullptr and useless.
-	// The ideal solution would be to use shared pointer or copy it as a raw pointer (Tedious) --
-	// but that's beyond the scope of this assignment.
-	void updatePathFollowAgent() {
-		if (nodePositions.size() == 0)
-			return;
-
-		if (currentNodeIndex < 0) currentNodeIndex = 0;
-		if (currentNodeIndex >= nodePositions.size()) currentNodeIndex = 0;
-		Vector2 target = nodePositions[currentNodeIndex];
-
-		if (Vector2Length(agent->position - target) > 10) agent->updateFrame(obj);
-		else {
-			// Increment, then check if it's time to reset, and only after update obj position
-			currentNodeIndex++;
-			if (currentNodeIndex >= nodePositions.size()) {
-				currentNodeIndex = 0;
-				nodePositions.clear();
-				return;
-			}
-			obj->position = nodePositions[currentNodeIndex];
-		}
-	}
-
-	void debugDraw() {
-		if (nodePositions.size() > 0) {
-			for (int i = 0; i < nodePositions.size(); i++) {
-				DrawCircleLines(nodePositions[i].x, nodePositions[i].y, 10, DARKPURPLE);
-
-				if (i < nodePositions.size() - 1) {
-					DrawLine(nodePositions[i].x, nodePositions[i].y,
-						nodePositions[i + 1].x, nodePositions[i + 1].y,
-						DARKPURPLE);
-				}
-			}
-		}
-	}
-
-	~PathfollowAgent() {
-		delete agent;
-		delete obj;
-	}
+	PathfollowAgent(int _maximumPathCount);
+	~PathfollowAgent();
+	void update();
+	void generateNewPath();
+	void updatePathFollowAgent();
+	void debugDraw();
 };
 
 // Avoid other moving into agents based on radius basically
@@ -111,66 +46,13 @@ struct SeparatedAgents {
 	const float agentRadius = 25.0f;
 	Player* trackedObject;
 
-	// Default constructor
-	SeparatedAgents() : numOfAgents(0), trackedObject(nullptr) {}
-	SeparatedAgents(int _numOfAgents) {
-		trackedObject = new Player(Vector2{ (float)GetScreenWidth() / 2, (float)GetScreenHeight() / 3 }, 25.0f, 5.0f);
+	SeparatedAgents();
+	SeparatedAgents(int _numOfAgents);
 
-		numOfAgents = _numOfAgents;
-		for (int i = 0; i < numOfAgents; i++) {
-			// Ugly but needed way to spawn agents a bit randomly 
-			// (If they spawn inside of each other then they'll stay like that)
-			Agent* agent;
-			agent = new Agent(Vector2{ (float)GetScreenWidth() / 2, (float)GetScreenHeight() / 2 + (agentRadius * (i + 1)) },
-				agentRadius, 3.0f, 0, 0.1f, true);
-
-			// Set agent behavior (Can vary per agent) (Default behavior is seek but we explicitly define it here)
-			agentList.push_back(agent);
-		}
-	}
-
-	virtual void update() {
-		handleCollision();
-		if (trackedObject) {
-			trackedObject->Update();
-		}
-		for (Agent* a : agentList) {
-			a->updateFrame(trackedObject);
-		}
-		trackedObject->Update();
-	}
-
-	virtual float getMinDistance(int dist1, int dist2) {
-		return (dist1 + dist2) * 2;
-	}
-
-	void handleCollision() {
-		for (int i = 0; i < agentList.size(); i++) {
-			for (int j = i + 1; j < agentList.size(); j++) {
-				Vector2 diff = agentList[i]->position - agentList[j]->position;
-				float distSq = diff.x * diff.x + diff.y * diff.y;
-
-				float minimumDistance = getMinDistance(agentList[i]->radius, agentList[j]->radius);
-				float minimumDistanceSqared = minimumDistance * minimumDistance;
-
-				if (distSq < minimumDistanceSqared) {
-					float dist = sqrtf(distSq);
-					Vector2 normal = diff / dist;
-
-					float penetration = (minimumDistance - dist) * 0.5f;
-					agentList[i]->position += normal * penetration;
-				}
-			}
-			agentList[i]->updateFrame(trackedObject);
-		}
-	}
-
-	virtual ~SeparatedAgents() {
-		delete trackedObject;
-		for (int i = 0; i < agentList.size(); i++) {
-			delete agentList[i];
-		}
-	}
+	virtual void update();
+	virtual float getMinDistance(int dist1, int dist2);
+	void handleCollision();
+	virtual ~SeparatedAgents();
 };
 
 // Initially I had the idea of abstracting away the walls into this 
@@ -178,7 +60,6 @@ struct SeparatedAgents {
 struct LineWall {
 	Vector2 start;
 	Vector2 end;
-
 
 	LineWall(Vector2 s, Vector2 e) : start(s), end(e) {};
 };
@@ -191,108 +72,50 @@ struct ObjectAvoidance : SeparatedAgents {
 
 	const int wallCount = 3;
 
-	ObjectAvoidance(int numAgents) : SeparatedAgents(numAgents) {
-		dummyObject = new Object(trackedObject->position, trackedObject->radius, 0);
-		// Walls
-		walls.push_back(LineWall({ 150, 150 }, {300, 150}));
-		walls.push_back(LineWall({ 500, 400 }, {500, 550}));
-		walls.push_back(LineWall({ 400, 700 }, {550, 750}));
-
-		// Box aka Object
-		walls.push_back(LineWall({ 800, 200 }, { 1000, 200 }));
-		walls.push_back(LineWall({ 1000, 200 }, { 1000, 350 }));
-		walls.push_back(LineWall({ 1000, 350 }, { 800, 350 }));
-		walls.push_back(LineWall({ 800, 350 }, { 800, 200 }));
-	}
-
-	~ObjectAvoidance() {
-		delete dummyObject;
-	}
-
-	void update() override {
-		avoidWalls();
-
-		SeparatedAgents::update();
-
-		trackedObject->Update();
-		trackedObject->drawShape();
-		drawWalls();
-	}
-
-	float getMinDistance(int dist1, int dist2) override {
-		return dist1 + dist2;
-	}
-
-	void avoidWalls() {
-		for (Agent* _a : agentList) {
-			float rayLength = 150.0f;
-			float fovAngle = 45.0 * DEG2RAD;
-			bool raycastHit = false;
-
-			Vector2 ray = _a->position + Vector2Normalize(_a->forwardDirection) * rayLength;
-			Vector2 forward = Vector2Normalize(_a->forwardDirection);
-			Vector2 avoidDir = forward;
-
-			// There are plenty of ways of doing this but i decided to go with 
-			// one bigger raycast in the center and two smaller ones
-			Vector2 whiskers[3] = {
-				_a->position + forward * rayLength,
-				_a->position + Vector2Rotate(forward, +fovAngle) * rayLength / 2,
-				_a->position + Vector2Rotate(forward, -fovAngle) * rayLength / 2,
-			};
-
-			for (int i = 0; i < walls.size(); i++) {
-				Vector2 mid = (walls[i].start + walls[i].end) * 0.5f;
-				float dist = Vector2Distance(ray, mid);
-
-				for (int i = 0; i < 3; i++) {
-					float dist = Vector2Distance(whiskers[i], mid);
-					if (dist < _a->radius + 50.0f) {
-						avoidDir = Vector2Normalize(_a->position - mid);
-						raycastHit = true;
-						break;
-					}
-				}
-				if (raycastHit) break;
-			}
-
-			if (raycastHit) {
-				// change target position
-				dummyObject->position = _a->position + avoidDir * rayLength;
-				_a->playerTarget = dummyObject;
-			}
-			else
-				_a->playerTarget = trackedObject;
-		}
-	}
-
-	void drawWalls() {
-		DrawText("- I understand that the agents don't perfectly avoid the box in particular. The main issue is separation sometimes overriding it",
-			200, 10, 20, RED);
-		DrawText("- I think potentially blending the behaviors with weights that the book brings up or just having real collision for the walls would work",
-			200, 40, 20, RED);
-		DrawText("- But if the agents are far enough from the player and move toward one of the box corners it works quite well",
-			200, 70, 20, RED);
-
-		for (int i = 0; i < walls.size(); i++) {
-			DrawLine(walls[i].start.x, walls[i].start.y, walls[i].end.x, walls[i].end.y, GREEN);
-		}
-	}
+	ObjectAvoidance(int numAgents);
+	~ObjectAvoidance();
+	void update() override;
+	float getMinDistance(int dist1, int dist2) override;
+	void avoidWalls();
+	void drawWalls();
 };
 
-//struct JumpingAgent {
-//	Agent* agent;
-//	Player* player;
-//
-//	JumpingAgent() {
-//		agent =  
-//	}
-//
-//	~JumpingAgent() {
-//		delete agent;
-//		delete player;
-//	}
-//};
+struct Pad {
+	Vector2 position;
+	Vector2 size;
+
+	Pad(Vector2 _position, Vector2 _size);
+	bool isAgentOnThisPad(Agent* agent, float padRadius);
+	void drawPad(Color _color);
+};
+
+// Basic agent with defualt behavior (Seek) which will jump over some edge to get to player
+// I didn't quite understand the nuance on what we had to do, but i just did something simple with a jumping pad
+// And check if the agent is on the jumping pad, then it will jump. Naturally this would probably be hidden.
+// Potentially it would make more sense to temporarily change the direction of the agent towards the jumping pad
+// But I wasn't sure if I should do that (It's quite easy to do and would just make it harder to test if the agent doesn't jump)
+struct JumpingAgent {
+	Agent* agent;
+	Player* player;
+	Pad* pad;
+	Pad* deathPad;
+	Vector2 jumpAcceleration;
+	float jumpSpeed;
+	float zPosition;
+	bool hasJumped;
+	float gravity;
+	float baseRadius;
+
+	JumpingAgent();
+	~JumpingAgent();
+
+	void jump();
+	void respawn();
+	void applyJumpPhysics();
+	void checkPads();
+	void drawPads();
+	void update();
+};
 
 // This is the struct that manages the above composed agents
 struct ComposedAgents {
@@ -301,44 +124,11 @@ struct ComposedAgents {
 	PathfollowAgent* pathFollowBehavior;
 	SeparatedAgents* separatedAgentsBehavior;
 	ObjectAvoidance* collisionAvoidanceBehavior;
-	//JumpingAgent* jumpingBehavior;
+	JumpingAgent* jumpingBehavior;
 
-	ComposedAgents() {
-		currentBehavior = Pathfollow;
-		pathFollowBehavior = new PathfollowAgent(5);
-		separatedAgentsBehavior = new SeparatedAgents(5);
-		collisionAvoidanceBehavior = new ObjectAvoidance(5);
-	}
+	ComposedAgents();
+	~ComposedAgents();
 
-	~ComposedAgents() {
-		delete pathFollowBehavior;
-		delete separatedAgentsBehavior;
-		delete collisionAvoidanceBehavior;
-	}
-
-	void displayDebug() {
-		switch (currentBehavior) {
-		case Pathfollow:
-			pathFollowBehavior->update();
-			DrawText("1/4 Type: Pathfollow", 10, GetScreenHeight() - 50, 20, RED);
-			break;
-		case AgentSeparation:
-			separatedAgentsBehavior->update();
-			DrawText("2/4 Type: Separated Agents Behavior", 10, GetScreenHeight() - 50, 20, RED);
-			break;
-		case CollisionAvoidance:
-			collisionAvoidanceBehavior->update();
-			DrawText("3/4 Type: Collision, agent and Wall Avoidance", 10, GetScreenHeight() - 50, 20, RED);
-			break;
-		default:
-			DrawText("Error, most likely invalid option picked", 10, GetScreenHeight(), 20, RED);
-			break;
-		}
-	}
-
-	void browseStates() {
-		if (IsKeyPressed(KEY_ONE)) currentBehavior = Pathfollow;
-		if (IsKeyPressed(KEY_TWO)) currentBehavior = AgentSeparation;
-		if (IsKeyPressed(KEY_THREE)) currentBehavior = CollisionAvoidance;
-	}
+	void displayDebug();
+	void browseStates();
 };
